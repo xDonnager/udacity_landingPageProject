857b4afb84be69bb5734068e621c5111491b57e9
PXI <paula.xicoy@enzymeadvisinggroup.com>
2022-08-27 13:25:20 +0200

    Feat: Adjusted scroll behavior with js, optimized navigation menu creation

--- js/app.js
+++ js/app.js
@@ -11,23 +11,25 @@
  * 
  * JS Standard: ESlint
  * 
-*/
+ */
 
 /**
  * Comments should be present at the beginning of each procedure and class.
  * Great to have comments before crucial code sections within the procedure.
-*/
+ */
 
 /**
  * Define Global Variables
  * 
-*/
+ */
 const sections = document.querySelectorAll('section');
 const navbarList = document.querySelector('#navbar__list');
 const header = document.querySelector('.page__header');
 const main = document.querySelector('main');
 
-/*Scrolling aux vars*/
+/**
+ * Scrolling aux vars
+ */
 let timeoutId;
 let scrollPos = 0;
 
@@ -37,7 +39,7 @@ let createdBtn = false;
  * End Global Variables
  * Start Helper Functions
  * 
-*/
+ */
 const hideNavMenu = () => {
     header.style.display = 'none'
 }
@@ -46,7 +48,7 @@ const showNavMenu = () => {
     header.style.display = 'initial'
 }
 
-function createScrollToTopButon(){
+function createScrollToTopButon() {
     const div = document.createElement('div');
     div.innerHTML = `<span class="material-symbols-outlined">arrow_upward</span> <a href="#hero">Scroll to top</a>`
     div.classList.add('scroll__btn');
@@ -59,11 +61,12 @@ function createScrollToTopButon(){
  * End Helper Functions
  * Begin Main Functions
  * 
-*/
+ */
 
 // build the nav
-function createListElements(sections){
-    let listElements = [];
+function createListElements(sections) {
+    const fragment = document.createDocumentFragment();
+
     for (const section of sections) {
         const listElement = document.createElement('li');
         const anchorElement = document.createElement('a');
@@ -73,22 +76,22 @@ function createListElements(sections){
         anchorElement.classList.add('menu__link');
 
         listElement.appendChild(anchorElement);
-        listElements.push(listElement)
+        fragment.appendChild(listElement);
     }
-    return listElements;
+    return fragment;
 }
 
-function createNavigationMenu(navItems, hostElement){
-    for(let i = 0; i < navItems.length; i++){
-        hostElement.appendChild(navItems[i])
-    }
+function createNavigationMenu(navItemsFragment, hostElement) {
+    hostElement.appendChild(navItemsFragment);
 }
 
 // Add class 'active' to section when near top of viewport
-function updateActiveSection(){
-    for (const section of sections){
+function updateActiveSection() {
+    for (const section of sections) {
         const rect = section.getBoundingClientRect();
-        if(rect.top >= 0 && rect.top <= 200 && rect.bottom >= 200){
+        console.log(section);
+        console.log(rect)
+        if (rect.top >= -30 && rect.top <= 200 && rect.bottom >= 200) {
             section.classList.add('focused')
         } else {
             section.classList.remove('focused')
@@ -97,44 +100,44 @@ function updateActiveSection(){
 }
 
 // Scroll to anchor ID using scrollTO event
-// function scrollToSection(e){
-//     console.log(e.target);
-//     // e.preventDefault();
-//     if(e.target.nodeName === 'A'){
-//         console.log('clicked A')
-//     }
-// }
+function scrollToSection(e) {
+    e.preventDefault();
+    const clickedSection = e.target.hash.replace('#', '')
+    const targetSection = document.getElementById(clickedSection);
+    targetSection.scrollIntoView({
+        block: 'start',
+        behavior: 'smooth'
+    })
+}
 
 
 /**
  * End Main Functions
  * Begin Events
  * 
-*/
+ */
 
-// Build menu 
+// Build menu and add click event listener
 document.addEventListener('DOMContentLoaded', () => {
     createNavigationMenu(createListElements(sections), navbarList);
-})
 
-// Scroll to section on link click
-// no need to implement event listeners, we are using html scroll-behavior: smooth;
-// navbarList.addEventListener('click', scrollToSection)
+    navbarList.addEventListener('click', scrollToSection)
+})
 
 // Set sections as active
 window.addEventListener('scroll', (e) => {
-    if(timeoutId) clearTimeout(timeoutId);
+    if (timeoutId) clearTimeout(timeoutId);
     scrollPos = window.scrollY;
 
     updateActiveSection();
     showNavMenu();
 
     //hide fixed navigation bar
-    timeoutId = setTimeout( ()=>{
+    timeoutId = setTimeout(() => {
         hideNavMenu();
     }, 3000)
 
-    if(scrollPos >= 2500 && !createdBtn) {
+    if (scrollPos >= 2500 && !createdBtn) {
         createScrollToTopButon();
         createdBtn = true;
     };
@@ -144,7 +147,7 @@ window.addEventListener('scroll', (e) => {
 
 //show navigation menu if mouse is near.
 window.addEventListener('mouseover', (e) => {
-    if(e.y <= 110){ 
+    if (e.y <= 110) {
         showNavMenu();
     }
 })
