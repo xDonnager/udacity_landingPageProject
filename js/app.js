22735049eeccd6e7244e4d605f948f6561383afe
PXI <paula.xicoy@enzymeadvisinggroup.com>
2022-08-28 17:36:16 +0200

    Style: passed JSLint

--- js/app.js
+++ js/app.js
@@ -27,11 +27,11 @@ const browserSupport = {
     selectors: !!document.querySelectorAll
 };
 const appSelectors = {
-    sections : document.querySelectorAll('section'),
-    navbarList : document.querySelector('#navbar__list'),
-    header : document.querySelector('.page__header'),
-    main : document.querySelector('main'),
-}
+    sections : document.querySelectorAll("section"),
+    navbarList : document.querySelector("#navbar__list"),
+    header : document.querySelector(".page__header"),
+    main : document.querySelector("main")
+};
 
 /**
  * Scrolling aux vars
@@ -47,26 +47,26 @@ let createdBtn = false;
  * 
  */
 const hideNavMenu = () => {
-    appSelectors.header.style.display = 'none'
-}
+    appSelectors.header.style.display = "none";
+};
 
 const showNavMenu = () => {
-    appSelectors.header.style.display = 'initial'
-}
+    appSelectors.header.style.display = "initial";
+};
 
 function createScrollToTopButon() {
-    const div = document.createElement('div');
-    div.innerHTML = `<span class="material-symbols-outlined">arrow_upward</span>`
-    div.classList.add('scroll__btn');
+    const div = document.createElement("div");
+    div.innerHTML = `<span class="material-symbols-outlined">arrow_upward</span>`;
+    div.classList.add("scroll__btn");
 
     // sync with hero section
-    div.addEventListener('click', (e) => {
+    div.addEventListener("click", (e) => {
         e.preventDefault();
-        document.getElementById('hero').scrollIntoView({
-            block: 'start',
-            behavior: 'smooth'
-        })
-    })
+        document.getElementById("hero").scrollIntoView({
+            block: "start",
+            behavior: "smooth"
+        });
+    });
 
     appSelectors.main.appendChild(div);
 }
@@ -83,46 +83,46 @@ function createListElements(sections) {
     const fragment = document.createDocumentFragment();
 
     for (const section of sections) {
-        const listElement = document.createElement('li');
-        const anchorElement = document.createElement('a');
+        const listElement = document.createElement("li");
+        const anchorElement = document.createElement("a");
 
-        anchorElement.textContent = section.getAttribute('data-nav');
-        anchorElement.href = '#' + section.id
-        anchorElement.classList.add('menu__link');
+        anchorElement.textContent = section.getAttribute("data-nav");
+        anchorElement.href = "#" + section.id;
+        anchorElement.classList.add("menu__link");
 
         listElement.appendChild(anchorElement);
         fragment.appendChild(listElement);
-    }
+    };
     return fragment;
-}
+};
 
 function createNavigationMenu(navItemsFragment, hostElement) {
     hostElement.appendChild(navItemsFragment);
-}
+};
 
-// Add class 'active' to section when near top of viewport
+// Add class "active" to section when near top of viewport
 function updateActiveSection(sections) {
     for (const section of sections) {
         const rect = section.getBoundingClientRect();
 
         if (rect.top >= -30 && rect.top <= 200 && rect.bottom >= 200) {
-            section.classList.add('focused')
+            section.classList.add("focused");
         } else {
-            section.classList.remove('focused')
-        }
-    }
-}
+            section.classList.remove("focused");
+        };
+    };
+};
 
 // Scroll to anchor ID using scrollTO event
 function scrollToSection(e) {
     e.preventDefault();
-    const clickedSection = e.target.hash.replace('#', '')
+    const clickedSection = e.target.hash.replace("#", "")
     const targetSection = document.getElementById(clickedSection);
     targetSection.scrollIntoView({
-        block: 'start',
-        behavior: 'smooth'
-    })
-}
+        block: "start",
+        behavior: "smooth"
+    });
+};
 
 
 /**
@@ -133,13 +133,14 @@ function scrollToSection(e) {
 
 if (browserSupport.events && browserSupport.selectors){
     // Build menu and add click event listener
-    document.addEventListener('DOMContentLoaded', () => {
-        createNavigationMenu(createListElements(appSelectors.sections), appSelectors.navbarList);
-        appSelectors.navbarList.addEventListener('click', scrollToSection)
+    document.addEventListener("DOMContentLoaded", () => {
+        const listElements = createListElements(appSelectors.sections);
+        createNavigationMenu(listElements, appSelectors.navbarList);
+        appSelectors.navbarList.addEventListener("click", scrollToSection);
     });
 
     // Set sections as active
-    window.addEventListener('scroll', (e) => {
+    window.addEventListener("scroll", (e) => {
         if (timeoutId) clearTimeout(timeoutId);
         scrollPos = window.scrollY;
 
@@ -157,16 +158,16 @@ if (browserSupport.events && browserSupport.selectors){
             createdBtn = true;
         };
 
-    })
+    });
 
     //show navigation menu if mouse is near.
-    window.addEventListener('mouseover', (e) => {
+    window.addEventListener("mouseover", (e) => {
         if (e.y <= 110) {
             showNavMenu();
-        }
-    })
+        };
+    });
 
 } else {
-    alert('Seems that you are using some kind of old browser, some features may not be available')
-}
+    alert("Some features may not be available due to browser compatibility");
+};
 
