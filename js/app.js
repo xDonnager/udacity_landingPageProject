ca3399175e3aa30a80f305d3bbe9ea89b0e2763a
PXI <paula.xicoy@enzymeadvisinggroup.com>
2022-08-28 17:40:15 +0200

    Styles: Format code with prettier

--- js/app.js
+++ js/app.js
@@ -1,16 +1,16 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
+ *
  */
 
 /**
@@ -20,17 +20,17 @@
 
 /**
  * Define Global Variables
- * 
+ *
  */
 const browserSupport = {
-    events: !!window.addEventListener,
-    selectors: !!document.querySelectorAll
+  events: !!window.addEventListener,
+  selectors: !!document.querySelectorAll,
 };
 const appSelectors = {
-    sections : document.querySelectorAll("section"),
-    navbarList : document.querySelector("#navbar__list"),
-    header : document.querySelector(".page__header"),
-    main : document.querySelector("main")
+  sections: document.querySelectorAll("section"),
+  navbarList: document.querySelector("#navbar__list"),
+  header: document.querySelector(".page__header"),
+  main: document.querySelector("main"),
 };
 
 /**
@@ -44,130 +44,125 @@ let createdBtn = false;
 /**
  * End Global Variables
  * Start Helper Functions
- * 
+ *
  */
 const hideNavMenu = () => {
-    appSelectors.header.style.display = "none";
+  appSelectors.header.style.display = "none";
 };
 
 const showNavMenu = () => {
-    appSelectors.header.style.display = "initial";
+  appSelectors.header.style.display = "initial";
 };
 
 function createScrollToTopButon() {
-    const div = document.createElement("div");
-    div.innerHTML = `<span class="material-symbols-outlined">arrow_upward</span>`;
-    div.classList.add("scroll__btn");
+  const div = document.createElement("div");
+  div.innerHTML = `<span class="material-symbols-outlined">arrow_upward</span>`;
+  div.classList.add("scroll__btn");
 
-    // sync with hero section
-    div.addEventListener("click", (e) => {
-        e.preventDefault();
-        document.getElementById("hero").scrollIntoView({
-            block: "start",
-            behavior: "smooth"
-        });
+  // sync with hero section
+  div.addEventListener("click", (e) => {
+    e.preventDefault();
+    document.getElementById("hero").scrollIntoView({
+      block: "start",
+      behavior: "smooth",
     });
+  });
 
-    appSelectors.main.appendChild(div);
+  appSelectors.main.appendChild(div);
 }
-
 
 /**
  * End Helper Functions
  * Begin Main Functions
- * 
+ *
  */
 
 // build the nav
 function createListElements(sections) {
-    const fragment = document.createDocumentFragment();
+  const fragment = document.createDocumentFragment();
 
-    for (const section of sections) {
-        const listElement = document.createElement("li");
-        const anchorElement = document.createElement("a");
+  for (const section of sections) {
+    const listElement = document.createElement("li");
+    const anchorElement = document.createElement("a");
 
-        anchorElement.textContent = section.getAttribute("data-nav");
-        anchorElement.href = "#" + section.id;
-        anchorElement.classList.add("menu__link");
+    anchorElement.textContent = section.getAttribute("data-nav");
+    anchorElement.href = "#" + section.id;
+    anchorElement.classList.add("menu__link");
 
-        listElement.appendChild(anchorElement);
-        fragment.appendChild(listElement);
-    };
-    return fragment;
-};
+    listElement.appendChild(anchorElement);
+    fragment.appendChild(listElement);
+  }
+  return fragment;
+}
 
 function createNavigationMenu(navItemsFragment, hostElement) {
-    hostElement.appendChild(navItemsFragment);
-};
+  hostElement.appendChild(navItemsFragment);
+}
 
 // Add class "active" to section when near top of viewport
 function updateActiveSection(sections) {
-    for (const section of sections) {
-        const rect = section.getBoundingClientRect();
+  for (const section of sections) {
+    const rect = section.getBoundingClientRect();
 
-        if (rect.top >= -30 && rect.top <= 200 && rect.bottom >= 200) {
-            section.classList.add("focused");
-        } else {
-            section.classList.remove("focused");
-        };
-    };
-};
+    if (rect.top >= -30 && rect.top <= 200 && rect.bottom >= 200) {
+      section.classList.add("focused");
+    } else {
+      section.classList.remove("focused");
+    }
+  }
+}
 
 // Scroll to anchor ID using scrollTO event
 function scrollToSection(e) {
-    e.preventDefault();
-    const clickedSection = e.target.hash.replace("#", "")
-    const targetSection = document.getElementById(clickedSection);
-    targetSection.scrollIntoView({
-        block: "start",
-        behavior: "smooth"
-    });
-};
-
+  e.preventDefault();
+  const clickedSection = e.target.hash.replace("#", "");
+  const targetSection = document.getElementById(clickedSection);
+  targetSection.scrollIntoView({
+    block: "start",
+    behavior: "smooth",
+  });
+}
 
 /**
  * End Main Functions
  * Begin Events
- * 
+ *
  */
 
-if (browserSupport.events && browserSupport.selectors){
-    // Build menu and add click event listener
-    document.addEventListener("DOMContentLoaded", () => {
-        const listElements = createListElements(appSelectors.sections);
-        createNavigationMenu(listElements, appSelectors.navbarList);
-        appSelectors.navbarList.addEventListener("click", scrollToSection);
-    });
+if (browserSupport.events && browserSupport.selectors) {
+  // Build menu and add click event listener
+  document.addEventListener("DOMContentLoaded", () => {
+    const listElements = createListElements(appSelectors.sections);
+    createNavigationMenu(listElements, appSelectors.navbarList);
+    appSelectors.navbarList.addEventListener("click", scrollToSection);
+  });
 
-    // Set sections as active
-    window.addEventListener("scroll", (e) => {
-        if (timeoutId) clearTimeout(timeoutId);
-        scrollPos = window.scrollY;
+  // Set sections as active
+  window.addEventListener("scroll", (e) => {
+    if (timeoutId) clearTimeout(timeoutId);
+    scrollPos = window.scrollY;
 
-        updateActiveSection(appSelectors.sections);
-        showNavMenu();
+    updateActiveSection(appSelectors.sections);
+    showNavMenu();
 
-        //hide fixed navigation bar
-        timeoutId = setTimeout(() => {
-            hideNavMenu();
-        }, 3000)
+    //hide fixed navigation bar
+    timeoutId = setTimeout(() => {
+      hideNavMenu();
+    }, 3000);
 
-        //create scrollup button
-        if (scrollPos >= 1000 && !createdBtn) {
-            createScrollToTopButon();
-            createdBtn = true;
-        };
+    //create scrollup button
+    if (scrollPos >= 1000 && !createdBtn) {
+      createScrollToTopButon();
+      createdBtn = true;
+    }
+  });
 
-    });
-
-    //show navigation menu if mouse is near.
-    window.addEventListener("mouseover", (e) => {
-        if (e.y <= 110) {
-            showNavMenu();
-        };
-    });
-
+  //show navigation menu if mouse is near.
+  window.addEventListener("mouseover", (e) => {
+    if (e.y <= 110) {
+      showNavMenu();
+    }
+  });
 } else {
-    alert("Some features may not be available due to browser compatibility");
-};
-
+  alert("Some features may not be available due to browser compatibility");
+}
